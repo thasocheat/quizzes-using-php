1d4a52058ca94eef2fae998ecef950c7d0efb131
thasocheat <thasocheat167@gmail>
2022-08-06 00:36:48 +0700

    update connection

--- database/connection.php
+++ database/connection.php
@@ -1,9 +1,9 @@
 <?php
 
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$dbname = "php_sample_quize_db";
+$dbhost = "ec2-44-198-82-71.compute-1.amazonaws.com";
+$dbuser = "fsmwnmuanqzsyk";
+$dbpass = "9010bf20aa8a13c538d7d8a1d4a81a0854687efa3ee4e21923f568c6f313e0aa";
+$dbname = "dcp5i9hfce4cqt";
 // $dbhost = "ec2-54-159-175-38.compute-1.amazonaws.com";
 // $dbuser = "dcjxsnywbmcass";
 // $dbpass = "7316e6d56c7471bee0861510d6e85d5d466a2c816bb16cc5c6df50e80f3cfdf4";
